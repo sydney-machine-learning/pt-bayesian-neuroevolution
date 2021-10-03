123bf3b0d79d047cc4aca73a49642e4d18c871fe
Arpit Kapoor <kapoor.arpit97@gmail.com>
2021-10-03 12:27:28 +0530

    Add plot weight distribution and trace NB

--- pt_pso/config/config.py
+++ pt_pso/config/config.py
@@ -33,7 +33,7 @@ parser.add_argument('--num-chains', type=int, default=10, help='Number of Chains
 parser.add_argument('--population-size', type=int, default=50, help='Population size for G3PCX Evolutionary algorithm.')
 parser.add_argument('--num-samples', type=int, default=50000, help='Total number of samples (all chains combined).')
 parser.add_argument('--swap-interval', type=int, default=100, help='Number of samples between each swap.')
-parser.add_argument('--burn-in', type=float, default=.10, help='Ratio of samples to be discarded as burn-in samples. Value 0.1 means 10 percent samples are discarded')
+parser.add_argument('--burn-in', type=float, default=.50, help='Ratio of samples to be discarded as burn-in samples. Value 0.1 means 10 percent samples are discarded')
 parser.add_argument('--max-temp', type=float, default=5, help='Temperature to be assigned to the chain with maximum temperature.')
 parser.add_argument('--topology', type=str, default=None, help='String representation of network topology. Eg:- "input,hidden,output"')
 parser.add_argument('--run-id', type=str, default=get_runid(datetime.now())
